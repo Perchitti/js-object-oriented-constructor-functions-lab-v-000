e789be0c9df8335f49c606c70175f743694bbe2a
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-20 10:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function Driver(name, age, experience){
   this.age = age;
   this.experience = experience;
 }
+
+let PickupLocation = (address, city) => {
+  this.address = address
+  this.city = city
+}
